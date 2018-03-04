70ea927712cc4c311ff26dbf268a331b0ed4e8bd
Juan Magana <juan.magana11@gmail.com>
2018-03-03 23:19:17 -0500

    Added Comments. Need to add error handling to text form and response from Giphy has zero content

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,60 +1,71 @@
+//'Global Variables'
+var button,
+	buttonVal,
+	encVal,
+	xhr,
+	enable,
+	src;
+
+//Prevents the default functionality of the form so it doesn't submit to itself. 
 $("form").submit(function(e){
 	e.preventDefault(e);
 });
 
+//handles the click on the add button or enter.
+$("#add").click(function(){
+	buildButton($("#textQuery").val());
+	cleanInput();
+	console.log($("#textQuery").val());
+});
+
+//handles the click on any of the created buttons.
+$(".queryButton").click(function(){
+	$("#grid").empty();
+	getGiphy(this);
+});
+
+//helper function to build a button. It gets the value of the button as an argument. 
 function buildButton (value){
-		var button = $('<button></button>', {
+		button = $('<button></button>', {
 						class: 'btn btn-outline-primary queryButton',
 						type: 'button',
 						text: value,
 						value: value, 
 						onclick: 'getGiphy(this);'
 						});
-			//button.addClass("btn btn-outline-primary");
-			//button.attr("type","button");
-			//button.attr("value", value);
-			$('#buttons').append(button);
+		//'appends the buttons to the buttons DIV'
+		$('#buttons').append(button);
 }
+
+//Cleans the Input Text
 function cleanInput (){
 	$("#textQuery").val("");
 }
 
-	$("#add").click(function(){
-		buildButton($("#textQuery").val());
-		cleanInput();
-		console.log($("#textQuery").val());
-	});
-
-
-$(".queryButton").click(function(){
-	$("#grid").empty();
-	getGiphy(this);
-});
-
+//calls Giphy API, creates image thumbnails and prepends it to the Grid Div tag.
 function getGiphy(e){
-	var buttonVal = $(e).val();
-	var encVal = encodeURIComponent(buttonVal);
-	var xhr = $.get("https://api.giphy.com/v1/gifs/search?api_key=jEqdCGWOY7yfKVXlHz4wmLGW4OdlMl6C&q=" + encVal);
+	buttonVal = $(e).val();
+	encVal = encodeURIComponent(buttonVal);
+	xhr = $.get("https://api.giphy.com/v1/gifs/search?api_key=jEqdCGWOY7yfKVXlHz4wmLGW4OdlMl6C&rating=g&q=" + encVal);
 	xhr.done(function(data) {
-
+		//'Loops thru the JSON object (data)'
 		for (var i = 0; i < data.data.length; i++){
-		 $('#grid').prepend("<img class='img-thumbnail img-responsive' src='" + data.data[i].images.fixed_height_still.url + "' />");
+		//'prepends an image per iteration'
+		 $('#grid').prepend("<img class='img-thumbnail img-responsive img-rounded' src='" + data.data[i].images.fixed_height_still.url + "' />");
 		}
+		//'handles the play/stop gif on all images prepended.'
+		enable = $('img').click(function(){
+	    	src = $(this).attr("src");
 
-		var toggle = false;
-		var enable = $('img').click(function(){
-	    	var src = $(this).attr("src");
+	    	if(src.indexOf("200_s.gif") > 1){
+	    		$(this).attr("src", src.replace(/200_s.gif/i, "200.gif"));
+	    		console.log($(this).attr("src"));
+	    	}
+	    	else{
+	    		$(this).attr("src", src.replace(/200.gif/i, "200_s.gif"));
+	    		console.log($(this).attr("src"));
+	    	}
+		});
 
-		    	if(src.indexOf("200_s.gif") > 1){
-		    		$(this).attr("src", src.replace(/200_s.gif/i, "200.gif"));
-		    		console.log($(this).attr("src"));
-		    	}
-		    	else{
-		    		$(this).attr("src", src.replace(/200.gif/i, "200_s.gif"));
-		    		toggle = true;
-		    		console.log($(this).attr("src"));
-		    	}
-			});
-
-    	});
+	});
 }
